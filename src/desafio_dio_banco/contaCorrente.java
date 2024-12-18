a5ab51b8af8ef6da2e7c34ac5881104e7ed69daa
dnvpimenta <dnvpimenta@gmail.com>
2024-12-18 10:46:00 -0300

    Commit teste

--- src/desafio_dio_banco/contaCorrente.java
+++ src/desafio_dio_banco/contaCorrente.java
@@ -7,7 +7,7 @@ public class contaCorrente extends conta {
 		
 	}
 	public void imprimirExtrato() {
-		System.out.println("=== Extrato conta corrente ===");
+		System.out.println("=== Extrato Conta Corrente ===");
 		super.imprimirInfosComuns();
 
 	}
